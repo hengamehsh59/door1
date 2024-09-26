69b64b4ffc55000ff01ccd197f42357d53c57980
hengamehsh59 <hengamehsh59@gmail.com>
2024-09-27 00:08:23 +0330

    Add files via upload

---
+++ main (2).cs
@@ -0,0 +1,34 @@
+class Door{
+    public:
+    char mat[b];
+    float x,y;
+    int student;
+    void open(){
+        status=1;//open
+    }
+    void close(){
+        status =0;//close
+    }
+    void lock(){
+        status=3;
+    }
+};
+int pin=123;
+bool cheakpin(intp){
+    if(pin==p){
+        status=2;
+    }
+}
+class lock:public Door{
+    private:
+    int pin=123;
+    public:
+    void lock(){
+        status=3;
+    }
+    void unlock(int p){
+        if(p==pin){
+            status=2;
+        }
+    }
+};
